c226ae947cc12cccba16a4f0ee6a43c0f4b0d720
Jonas Souza <jonasmzsouza@gmail.com>
2024-03-01 22:03:45 -0300

    add crud in memory with express

--- server.js
+++ server.js
@@ -1,18 +1,7 @@
-const http = require("http");
-const port = 3000;
+import app from "./src/app.js";
 
-const rotes = {
-  "/": "Curso de Node",
-  "/livros": "Listagem de livros",
-  "/autores": "Listagem de autores",
-  "/editora": "Listagem de editoras",
-};
+const port = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end(rotes[req.url]);
-});
-
-server.listen(port, () => {
+app.listen(port, () => {
   console.log(`Servidor escutando em http://localhost:${port}`);
 });
