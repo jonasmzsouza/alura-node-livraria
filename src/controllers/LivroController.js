9010364d0da0342192ae63777d45d63f3afb109f
Jonas Souza <jonasmzsouza@gmail.com>
2024-03-03 13:13:31 -0300

    add autor schema

--- src/controllers/LivroController.js
+++ src/controllers/LivroController.js
@@ -51,7 +51,7 @@ class LivroController {
 
     livros.findByIdAndDelete(id, (err) => {
       if (!err) {
-        res.status(200).send({ message: "Livro removido com sucewsso!" });
+        res.status(200).send({ message: "Livro removido com sucesso!" });
       } else {
         res.status(500).send({ message: err.message });
       }
